82ad72a1a3b0139df3588eb5a25d1f9a7e844755
Frances Chang <fchang@Francess-MacBook-Air.local>
2025-05-08 15:07:08 -0400

    added signup and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import AddPantryItem from './components/AddPantryItem';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Recipes from './pages/Recipes';
+import List from './pages/List';
+import Landing from './pages/Landing';
+import Account from './pages/Account';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>🔍 PantryPal</h1>
-        <nav>
-          <a href="#pantry">My Pantry</a>
-          <a href="#recipes">Recipes</a>
-          <a href="#shopping">Shopping Lists</a>
-          <a href="#account">Account</a>
-        </nav>
-      </header>
-      <main>
-        <AddPantryItem />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Top Header and Navigation */}
+        <header className="App-header">
+          <h1><Link to="/">🔍 PantryPal</Link></h1>
+          <nav>
+            <Link to="/pantry">My Pantry</Link>
+            <Link to="/recipes">Recipes</Link>
+            <Link to="/shopping">Shopping Lists</Link>
+            <Link to="/account">Account</Link>
+            <Link to="/login">Login</Link>
+          </nav>
+        </header>
+
+        {/* Page Routing */}
+        <main>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/pantry" element={<AddPantryItem />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/account" element={<Account />} />
+            {/* <Route path="/" element={<Landing />} /> */}
+            <Route path="/" element={<div>Welcome to PantryPal!</div>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
   
